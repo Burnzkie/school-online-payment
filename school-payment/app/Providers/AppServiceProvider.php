8bd1abe00419c62f6317e7fe688622ad545f6958
Burnzkie <judeburnea20@gmail.com>
2026-03-29 15:20:53 +0800

    force HTTPS in production

--- school-payment/app/Providers/AppServiceProvider.php
+++ school-payment/app/Providers/AppServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-       
-}
+        // Force HTTPS in production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
