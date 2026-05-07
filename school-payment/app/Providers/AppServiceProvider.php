f6be75a9f1ad2a96e438aad50ea205decd50cfb1
Burnzkie <judeburnea20@gmail.com>
2026-05-07 21:08:38 +0800

    Trust proxies for Render deployment

--- school-payment/app/Providers/AppServiceProvider.php
+++ school-payment/app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
 
         // Force HTTPS in production
         if (config('app.env') === 'production') {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
